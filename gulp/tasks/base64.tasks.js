14d0e7f45d5a7909b9f3b73d9c0c4838f1270be3
baibai <1871015627@qq.com>
2016-11-24 14:28:25 +0800

    更新readme.md

--- gulp/tasks/base64.tasks.js
+++ gulp/tasks/base64.tasks.js
@@ -1,6 +1,5 @@
 module.exports = function (gulp, config, $, args) {
     //----------------base64----------------
-
     /************在css文件有@import的情况下请先合并再执行该命令************************/
     gulp.task("Base64:NoRun",function(){
         return gulp.src(config.css)
